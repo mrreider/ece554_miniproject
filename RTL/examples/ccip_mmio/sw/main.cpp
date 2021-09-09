1edc10446ad9937c7317d4e844bf5146396ecac8
mrreider <rbrostoff@wisc.edu>
2021-09-09 17:35:24 -0500

    Update main.cpp

--- RTL/examples/ccip_mmio/sw/main.cpp
+++ RTL/examples/ccip_mmio/sw/main.cpp
@@ -57,8 +57,8 @@ int main(int argc, char *argv[]) {
     for (uint64_t i=0; i < 100; i++) {
       afu.write(USER_REG_ADDR, i);
       uint64_t result = afu.read(USER_REG_ADDR);
-
-      if (result != i) {
+      
+      if (result != (i-7) && i > 6) {
 	cerr << "ERROR: Read from MMIO register has incorrect value " << result << " instead of " << i << endl;
 	errors ++;
       }
